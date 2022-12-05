3a7f00c3fbbd7889baaae401ad4dd98bb4e5db19
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2022-12-05 14:31:01 -0500

    migration

--- contracts/deposit-cw20/src/contract.rs
+++ contracts/deposit-cw20/src/contract.rs
@@ -389,8 +389,9 @@ pub fn query(deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
-pub fn migrate(_deps: DepsMut, _env: Env, _msg: MigrateMsg) -> Result<Response, ContractError> {
-    unimplemented!()
+pub fn migrate(deps: DepsMut, _env: Env, _msg: MigrateMsg) -> Result<Response, ContractError> {
+    cw_utils::ensure_from_older_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
+    Ok(Response::default())
 }
 
 pub fn receive_cw20(
